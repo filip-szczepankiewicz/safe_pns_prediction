4a9e7d0874858cf872c3aa5258297b3cdca8bb58
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2019-07-28 20:09:11 -0400

    Update safe_rotmat_random.m

--- safe_rotmat_random.m
+++ safe_rotmat_random.m
@@ -1,7 +1,9 @@
 function R = safe_rotmat_random()
 % function R = safe_rotmat_random()
-%
 % Function returns a random rotation matrix (3x3).
+% For more information see:
+% How to generate a random unitary matrix, by Maris Ozols (March 16, 2009)
+% http://home.lu.lv/~sd20008/papers/essays/Random%20unitary%20[paper].pdf
 
 [q, r] = qr(randn(3));
 R = q*diag(sign(diag(r)));
