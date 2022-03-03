8def989c32b20feb236a7ea22aca8f7ae4565404
filip-szczepankiewicz <filip.szczepankiewicz@gmail.com>
2022-03-03 10:00:36 +0100

    Update safe_hw_check.m
    Added check for empty fields

--- safe_hw_check.m
+++ safe_hw_check.m
@@ -7,5 +7,21 @@ if (abs(hw.x.a1 + hw.x.a2 + hw.x.a3 - 1) > 0.001 || ...
     abs(hw.y.a1 + hw.y.a2 + hw.y.a3 - 1) > 0.001 || ...
     abs(hw.z.a1 + hw.z.a2 + hw.z.a3 - 1) > 0.001)
 
-    error('a1+a2+a3 must be equal to 1!')
+    error('Hardware specification a1+a2+a3 must be equal to 1!')
+end
+
+axl = {'x', 'y', 'z'};
+
+for i = 1:numel(axl)
+    axn = axl{i};
+    fnl = fieldnames(hw.(axn));
+
+    for j = 1:numel(fnl)
+        par = fnl{j};
+        val = hw.(axn).(par);
+
+        if isempty(val)
+            error(['Hardware specification ' axn '.' par ' is empty!'])
+        end
+    end
 end
