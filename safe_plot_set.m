d435a630c47e7952af331177c2a515f1a4977ce8
FilipSz <filip.szczepankiewicz@gmail.com>
2024-10-10 11:26:08 +0200

    updates

--- safe_plot_set.m
+++ safe_plot_set.m
@@ -8,7 +8,13 @@ w = floor(sqrt(n));
 
 for i = 1:n
 
-    pns = safe_gwf_to_pns(gwf{i}, rf{i}, dt{i}, hw, doPadding);
+    tmp = gwf{i};
+
+    if rank(tmp) < 2
+        tmp = tmp(:,1) * [1 1 1];
+    end
+
+    pns = safe_gwf_to_pns(tmp, rf{i}, dt{i}, hw, doPadding);
 
     subplot(h, w, i)
     safe_plot(pns, dt{i})
